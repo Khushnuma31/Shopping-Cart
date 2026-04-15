7e81d8c9d15f69a9f373f91e0a8394848ccd41d5
Kuldeep Jadeja <kuldeepsinh.jadeja@thegatewaycorp.co.in>
2026-04-15 14:57:38 +0530

    fix: update CORS origin configuration for production and development

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ connectDb()
 
 app.use(express.json())
 app.use(cors({
-    origin:[process.env.ORIGIN,'https://shopping-cart-mern-yo9j.vercel.app'],
+    origin: process.env.ORIGIN ? process.env.ORIGIN.split(',') : ['http://localhost:5173'],
     credentials:true
 }))
 app.use(cookieParser())
